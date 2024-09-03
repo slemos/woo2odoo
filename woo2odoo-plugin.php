913256fab18359db4f42d3b0e0f3b58cad3e94c7
Sebastian Lemos <slemos.satue@gmail.com>
2024-09-02 22:27:00 -0300

    Prune orphan files. Added new fields in settings

--- woo2odoo-plugin.php
+++ woo2odoo-plugin.php
@@ -24,12 +24,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once 'classes/class-woo2odoo-plugin.php';
 
 /**
+ * WooCommerce fallback notice.
+ *
+ * @since 0.1.0
+ */
+function woo2odoo_missing_wc_notice() {
+	/* translators: %s WC download URL link. */
+	echo '<div class="error"><p><strong>' . sprintf( esc_html__( 'Woo2odoo requires WooCommerce to be installed and active. You can download %s here.', 'woo2odoo-plugin' ), '<a href="https://woo.com/" target="_blank">WooCommerce</a>' ) . '</strong></p></div>';
+}
+
+/**
  * Returns the main instance of Woo2odoo_Plugin to prevent the need to use globals.
  *
  * @since  1.0.0
  * @return object Woo2odoo_Plugin
  */
 function woo2odoo_plugin() {
+	load_plugin_textdomain( 'woo2odoo', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+	
+	if ( ! class_exists( 'WooCommerce' ) ) {
+		add_action( 'admin_notices', 'woo2odoo_missing_wc_notice' );
+		return;
+	}
+
 	return Woo2Odoo_Plugin::instance();
 }
 add_action( 'plugins_loaded', 'woo2odoo_plugin' );
